2e9df340cae0edade0c57aba86498e96c10742b2
ABHISHEK SINGH <abhisingh11644@gmail.com>
2026-03-29 13:06:05 +0530

    Finalizing netlify serverless configuration

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,4 +1,7 @@
 process.env.SERVERLESS = 'true';
+process.env.SUPABASE_URL = 'https://rbbirmoxbcjaollxxxxk.supabase.co';
+process.env.SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJiYmlybW94YmNqYW9sbHh4eHhrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ3Njc0NzMsImV4cCI6MjA5MDM0MzQ3M30.VXIAmvUkK0EXoqIsD9NM7Ek7zJ4JXEUOLTI3Kn4unO0';
+process.env.SUPABASE_SERVICE_ROLE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJiYmlybW94YmNqYW9sbHh4eHhrIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc3NDc2NzQ3MywiZXhwIjoyMDkwMzQzNDczfQ.ADM1RKBnL9zALfYqy5S4OC0oN2pK8Nc60wX1jYrwMg4';
 
 const serverless = require('serverless-http');
 const app = require('../../server');
